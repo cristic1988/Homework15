2c5631795929e91606ef4830418970a8ea8523e9
cristic1988 <cristic8@gmail.com>
2024-04-16 22:33:31 +0300

    second commit

--- src/test/java/MatrixTest.java
+++ src/test/java/MatrixTest.java
@@ -17,11 +17,42 @@ class MatrixTest {
     @Test
     void avgArray() {
         Matrix matrix = new Matrix();
-        assertEquals( (double).matrix , matrix.avgArray());
-
+//        int[] array = new int[];
+//        assertEquals(matrix.avgArray(array));
     }
 
     @Test
     void isSquareMatrix() {
+        Matrix matrix = new Matrix();
+        int[][] matrix1 = new int[5][5];
+        assertTrue(matrix.isSquareMatrix(matrix1));
+
+    }
+
+    @Test
+    void isSquareMatrixTeeth() {
+        Matrix matrix = new Matrix();
+        int[][] matrix2 = new int[][]{{1, 2, 3}, {1, 2, 3, 4}, {1, 2, 3}};
+        assertFalse(matrix.isSquareMatrix(matrix2));
+
+    }
+
+    @Test
+    void isSquareMatrixZero() {
+        Matrix matrix = new Matrix();
+        int[][] matrix3 = new int[0][0];
+        assertFalse(matrix.isSquareMatrix(matrix3));
+    }
+    @Test
+    void isSquareMatrixNull() {
+        Matrix matrix = new Matrix();
+        int[][] matrix4 = null;
+        assertFalse(matrix.isSquareMatrix(matrix4));
+    }
+    @Test
+    void isSquareMatrixNo() {
+        Matrix matrix = new Matrix();
+        int[][] matrix5 = new int[5][8];
+        assertFalse(matrix.isSquareMatrix(matrix5));
     }
 }
